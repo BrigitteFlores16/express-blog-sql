0bddefe0c6b48686f27281d3af4141b396465da0
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-01-09 17:36:20 +0100

    Milestone 4

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,14 +15,21 @@ function index(req, res) {
 
 //# show
 function show(req, res) {
-  //const id= parseInt (req.params.id);
-  //const post = postsdata.find((post)=> post.id === id);
-  //if(!post){
-  //const err = new Error('posts not found');
-  //err.code= 404;
-  //throw err;
-  //};
-  // res.json (post);
+  const id = parseInt(req.params.id);
+  const sql = "SELECT * FROM `posts` WHERE `id` = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Database query failed" });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: "Posts not found" });
+    }
+
+    res.json(results[0]);
+  });
 }
 
 //# store
